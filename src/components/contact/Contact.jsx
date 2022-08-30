8682c8d65a1ca4729770e32af40e1a41a55db267
Aurelien <maltaverneaurelien@gmail.com>
2022-08-30 23:57:51 +0200

    modif click here arialabel

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -26,19 +26,19 @@ const Contact = () => {
             <MdOutlineEmail />
             <h4>Email</h4>
             <h5>maltaverneaurelien@gmail.com</h5>
-            <a href="mailto:maltaverneaurelien@gmail.com" target="_blank">Send a message</a>
+            <a href="mailto:maltaverneaurelien@gmail.com" target="_blank" aria-label="Send Mail">Send a message</a>
           </article>
           <article className="contact__option">
             <FaGithub />
             <h4>Github</h4>
             <h5>https://github.com/MaltaverneAurelien</h5>
-            <a href="https://github.com/MaltaverneAurelien" target="_blank">Click here</a>
+            <a href="https://github.com/MaltaverneAurelien" target="_blank" aria-label="Github">Click here</a>
           </article>
           <article className="contact__option">
             <BsLinkedin />
             <h4>LinkedIn</h4>
             <h5>https://www.linkedin.com/in/aurélien-maltaverne/</h5>
-            <a href="https://www.linkedin.com/in/aur%C3%A9lien-maltaverne" target="_blank">Click here</a>
+            <a href="https://www.linkedin.com/in/aur%C3%A9lien-maltaverne" target="_blank" aria-label="Linkedin">Click here</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
